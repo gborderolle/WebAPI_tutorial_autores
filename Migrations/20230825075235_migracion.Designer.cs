35a02bb160825f75319e2eb19a5f47e6161352b7
Gonzalo Borderolle <gborderolle@gmail.com>
2023-09-11 01:55:16 -0300

    1

--- Migrations/20230825075235_migracion.Designer.cs
+++ Migrations/20230825075235_migracion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAPI_tutorial_recursos.Context;
 
@@ -11,9 +12,11 @@ using WebAPI_tutorial_recursos.Context;
 namespace WebAPI_tutorial_autores.Migrations
 {
     [DbContext(typeof(ContextDB))]
-    partial class ContextDBModelSnapshot : ModelSnapshot
+    [Migration("20230825075235_migracion")]
+    partial class migracion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
